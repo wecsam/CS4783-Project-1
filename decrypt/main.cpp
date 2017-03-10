656daf47ca7c545b466e89089e7d71871cdaebec
wecsam <david@davidtsai.net78.net>
2017-03-09 22:13:17 -0500

    Convert user input directly to int
    Instead of splitting the input into a vector of strings, the program
    splits it into a vector of ints directly.

--- decrypt/main.cpp
+++ decrypt/main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <map>
 #include <vector>
 using namespace std;
 
 // Splits a string based off of a delimiter
-template<typename Out>
-void split(const string &s, char delim, Out result) {
-    stringstream ss;
-    ss.str(s);
-    string item;
-    while (getline(ss, item, delim)) {
-        *(result++) = item;
-    }
-}
-
-// Driver for string splitting
-vector<string> split(const string &s, char delim) {
-    vector<string> v;
-    split(s, delim, back_inserter(v));
-    return v;
+bool integerCsvToVector(const string& rowstr, vector<int>& result){
+	// This function converts a string of comma-separated values into a vector.
+	// It returns true on success and false on failure.
+	// If the vector is not empty, the values are pushed onto the end.
+	stringstream ss(rowstr);
+	if(!ss){
+		return false;
+	}
+	string cell;
+	while(getline(ss, cell, ',')){
+		result.push_back(stoi(cell));
+	}
+	return true;
 }
 
 int main(){
@@ -30,19 +27,20 @@ int main(){
         cerr << "Enter the encrypted message: ";
         getline(cin, input);
     } while(!input.length());
+	
+	// TODO: Make sure that the input is all commas and spaces.
+	// Split input by the commas and convert to int.
+    vector<int> ciphertext;
+	ciphertext.reserve(500);
+	if(!integerCsvToVector(input, ciphertext)){
+		cerr << "A stringstream could not be initialized.\n";
+		return 2;
+	}
     
     // Verify that there are 500 encrypted characters
-    int count = 0;
-    for(char c : input) {
-        if(c == ',') {
-            count++;
-        }
-    }
-    // Enforce the 500 encrypted characters rule
-//    if(count != 499) {
+//    if(ciphertext.size() != 500) {
 //        cerr << "The provided ciphertext is not L = 500.\n";
 //        return 1;
 //    }
-    vector<string> ciphertext = split(input, ',');
 	return 0;
 }
