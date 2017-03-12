dbca2463c61d1bc79c45945de348827bda3a0e6f
wecsam <david@davidtsai.net78.net>
2017-03-12 05:24:08 -0400

    Created AverageFrequency()
    This function returns a distribution of English letters and the number
    of key values that each of them gets. This information was taken from
    the project instructions.

---
+++ decrypt/AverageFrequency.h
@@ -0,0 +1,4 @@
+#include <unordered_map>
+// Returns a map of how many key values each English letter gets.
+// This information was taken straight from the project instructions.
+std::unordered_map<char, int> AverageFrequency();
